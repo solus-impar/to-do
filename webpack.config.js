db8a055bf77fc3474e612bf84f63a58e681f9765
Mike Canoy <canoym@wwu.edu>
2018-03-18 01:30:10 -0700

    Add ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,15 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }.
-      {
-        test: /\.scss$/,
-        loader: 'sass-loader'
-      }
-    ]
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  } 
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/,
+        uses: [ 'babel-loader', 'eslint-loader' ] },
+      { test: /\.scss$/, loader: 'sass-loader'}
+    ]
+  },
 };
